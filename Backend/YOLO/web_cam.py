dbe06506d3069f88426786cc0cd97b352c9c1c2c
yashas-super-boss-77 <yashashm77@gmail.com>
2023-03-04 07:00:15 +0530

    new updates

--- Backend/YOLO/web_cam.py
+++ Backend/YOLO/web_cam.py
@@ -1,31 +1,52 @@
 import cv2, os
+import time
 
 cap = cv2.VideoCapture(0)
 
-list_dir = os.listdir("D:\\Projects\\ISL using yolov8\\Backend\\YOLO\\data_source")
+list_dir = os.listdir("D:\\Projects\\ISL using yolov8\\Backend\\YOLO\\training_data\\words\\namaste")
 
 list_dir = [int(x[:-5]) for x in list_dir]
 
 flag = True
+count = 0
+
+start_point = (100, 100)
+
+stop_point = (740, 740)
 
 while (True):
     success, image = cap.read()
 
-    image = cv2.resize(image, (640, 640))
+    image = cv2.rectangle(image, start_point, stop_point, 4)
 
-    #Convert an image from BGR to grayscale mode 
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-   
-    #Convert a grayscale image to black and white using binary thresholding 
-    (thresh, BnW_image) = cv2.threshold(gray_image, 125, 255, cv2.THRESH_BINARY)
+    cv2.imshow("output", image)
+
+    image = image[100:740, 100:740]
+
+    cv2.imshow("output", image)
 
     if (flag == True):
+        
+        print ("position your hand")
+        while (count != 10):
+
+            time.sleep(2)
+            print (count)
+            count += 1
+
         flag = False
         if len(list_dir) == 0:
             image_name = 1
         else:
             image_name = max(list_dir) + 1
+    
+    image = cv2.resize(image, (128, 128))
 
-    cv2.imwrite("D:\\Projects\\ISL using yolov8\\Backend\\YOLO\\data_source\\"+str(image_name)+".jpeg", BnW_image)
+    cv2.imwrite("D:\\Projects\\ISL using yolov8\\Backend\\YOLO\\training_data\\words\\namaste\\nam_"+str(image_name)+".jpeg", image)
 
     image_name = image_name + 1
+    if image_name > 1000:
+        break
+
+    if cv2.waitKey(5) & 0xFF == 27:
+        break
--- GUI/dashboard.py
+++ GUI/dashboard.py
@@ -1,19 +1,37 @@
 from tkinter import ttk
 from tkinter import *
 import tkinter as tk
-
-
+import cv2
+from PIL import ImageTk, Image
+import sys
+sys.path.append("Backend/YOLO/ultralytics/")
+import yolo_run
 
 def create_dashboard():
+    def start_yolo():
+        root.configure(bg = "yellow")
+        yolo_run.run_yolo()
+        root.configure(bg = "white")
+
+
     root = Tk()
 
     root.title("ISL using YOLOv8 and word recoginition")
+
+    # root.geometry("1024x1024")
+
     
     # set the window to full screen
     root.attributes("-fullscreen", True)
 
-    title_frame = tk.Frame(root, height = 700, width = 800,  bg = 'black',  relief = 'sunken')
-    title_frame.place(x = 50, y = 100)
+    start_button = Button(root, text = "START", font = ("rockwell", 30))
+    start_button.place(relx=0.5, rely=0.5, anchor=CENTER)
+    start_button['command'] = start_yolo
+    
+    cap = cv2.VideoCapture(0)
+
+    success, img = cap.read()   
+
 
     dashboard_label = tk.Label(root, text="Indian Sign Language using YOLOv8", font= ("Rockwell", 20))
     dashboard_label.pack(side = TOP)
